400508c9cb3b61ffecf40e164da5a0ae4e838dae
Murtaza <murtaza.ml.lakhani@gmail.com>
2024-09-18 20:25:11 +0200

    optamize 7

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 
 import { navItems } from "@/data";
 
-//import Hero from "@/components/Hero";
-import Grid from "@/components/Grid";
+import Hero from "@/components/Hero";
+//import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
@@ -16,7 +16,7 @@ const Home = () => {
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
-        <Grid />
+        <Hero />
         <RecentProjects />
         <Experience />
         <Approach />
