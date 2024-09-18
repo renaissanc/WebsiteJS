e9935430ef39122996bde4d09104354c2c18033e
Murtaza <murtaza.ml.lakhani@gmail.com>
2024-09-18 20:52:03 +0200

    optamize 10

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -8,7 +8,7 @@ import { cn } from "@/lib/utils";
 
 
 import { BackgroundGradientAnimation } from "./GradientBg";
-//import GridGlobe from "./GridGlobe";
+import GridGlobe from "./GridGlobe";
 import animationData from "@/data/confetti.json";
 import MagicButton from "../MagicButton";
 import Image from "next/image";
@@ -133,7 +133,7 @@ export const BentoGridItem = ({
             {title}
           </div>
 
-          {/*id === 2 && <GridGlobe />*/}
+          {id === 2 && <GridGlobe />}
 
           {id === 3 && (
             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
