4c87e557babc1289f4466567f17e81d5d6ad0e6a
Murtaza <murtaza.ml.lakhani@gmail.com>
2024-09-18 18:41:20 +0200

    optamize 3

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@
 
 import { navItems } from "@/data";
 
-import Hero from "@/components/Hero";
-import Grid from "@/components/Grid";
+//import Hero from "@/components/Hero";
+//import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
 import Approach from "@/components/Approach";
-import Experience from "@/components/Experience";
+//import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Image from "next/image";
@@ -16,10 +16,7 @@ const Home = () => {
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
-        <Hero />
-        <Grid />
         <RecentProjects />
-        <Experience />
         <Approach />
         <Image src="/Resume.jpg" alt="Resume" width={1000} height={1000} style={{
           display: "block",
