ad36856e895dd851f34f689a42605d872dddfdf9
Murtaza <murtaza.ml.lakhani@gmail.com>
2024-09-20 10:38:33 +0200

    opt 20/10/24 -1

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
-import Grid from "@/components/Grid";
+//import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
