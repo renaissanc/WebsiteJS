6ceac6baf9d6e622876887d0f8c24a8e0c55d9ef
Murtaza <murtaza.ml.lakhani@gmail.com>
2024-09-20 11:19:59 +0200

    opt 20/10/24 -3

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 
 import "./globals.css";
 import { ThemeProvider } from "./provider";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
