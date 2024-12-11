1b534f474bb0079c37d5dec85b031c32f7926af0
Murtaza <murtaza.ml.lakhani@gmail.com>
2024-12-11 23:04:06 +0100

    first

---
+++ app/Rag_bot/page.tsx
@@ -0,0 +1,9 @@
+import { IoBody } from "react-icons/io5";
+
+export default function Rag_bot() {
+    return (
+        <div className="flex justify-center items-center h-screen pt-200">
+            some text
+        </div>
+    );
+}
